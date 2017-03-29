b7f00823d2c3b305769c7c921c21b9fbf001972e
zhonghao <hao.zhong@winasia.io>
2017-03-29 17:41:18 +0800

    提交分支

--- src/main/java/winasia/server/MainServer.java
+++ src/main/java/winasia/server/MainServer.java
@@ -1,22 +1,11 @@
 package winasia.server;
 
 import io.vertx.core.*;
-import io.vertx.core.eventbus.EventBus;
-import io.vertx.core.eventbus.impl.EventBusImpl;
-import io.vertx.core.http.HttpServer;
-import io.vertx.core.http.HttpServerRequest;
-import io.vertx.core.http.HttpServerResponse;
-import io.vertx.core.http.WebSocket;
-import io.vertx.ext.web.Router;
-import io.vertx.ext.web.templ.ThymeleafTemplateEngine;
-//import winasia.service.IndexService;
-//import winasia.service.PostForm;
-import winasia.staticresource.StaticData;
+
 import winasia.util.Runner;
 
-import java.util.function.Consumer;
 
-/**
+/*
  * Created by WinAsia on 2017/3/27.
  */
 public class MainServer extends AbstractVerticle {
@@ -27,7 +16,25 @@ public class MainServer extends AbstractVerticle {
 
     @Override
     public void start() throws Exception {
-        EventBus eventBus = vertx.eventBus();
         Future<Integer> future = Future.future();
+        WorkerExecutor executor = vertx.createSharedWorkerExecutor("work");
+        executor.executeBlocking(f ->{
+            f.complete(999);
+        }, future.completer());
+
+        /*
+        vertx.executeBlocking(handler -> {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            handler.complete(999);
+        }, future.completer());
+        */
+        Future<Integer> f = future.map(i -> {
+            return i + 1;
+        });
+        System.out.println(f.result());
     }
 }
